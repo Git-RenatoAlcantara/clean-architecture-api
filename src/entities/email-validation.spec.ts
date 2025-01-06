bf8b7905c8ceeeb72e5a95ba4dcbeb936f55c077
Renato A. dos Santos <renato.forjob@gmail.com>
2025-01-06 13:21:05 -0300

    test: validate eails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,6 +16,12 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeTruthy();
   });
 
+  test('should not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy();
+  });
+
+
   test('should not accept null strings', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy();
