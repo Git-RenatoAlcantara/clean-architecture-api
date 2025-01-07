e5c3c0ec06a19fc158a6afa0d426e85efb069b28
Renato A. dos Santos <renato.forjob@gmail.com>
2025-01-06 21:26:54 -0300

    feat: validate email with domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,5 +1,6 @@
 export class Email {
    static validate(email: string): boolean {
+    const MAX_DOMAIN_LENGTH = 63
        if(!email){
         return false
        }
@@ -9,11 +10,16 @@ export class Email {
        }
 
        const [local, domain] = email.split('@')
-       console
+    
        if(local.length > 64 || local.length === 0){
         return false
        }
        if(domain.length > 255 || domain.length === 0){
+        return false
+       }
+       const domainParts = domain.split('.')
+       const isDomainMaxLength = domainParts.some(part => part.length > MAX_DOMAIN_LENGTH)
+       if(isDomainMaxLength){
         return false
        }
        return true;
